fc8329584d75e0433b71976a1b8c5ff64cc0fc3d
conecteup <116357802+conecteup@users.noreply.github.com>
2024-02-18 14:37:43 -0300

    adas

--- frontend/src/hooks/useSettings/index.js
+++ frontend/src/hooks/useSettings/index.js
@@ -1,37 +1,29 @@
 import api from "../../services/api";
 
 const useSettings = () => {
-  const getAll = async (params) => {
-    const { data } = await api.request({
-      url: "/settings",
-      method: "GET",
-      params,
-    });
-    return data;
-  };
 
-  const update = async (data) => {
-    const { data: responseData } = await api.request({
-      url: `/settings/${data.key}`,
-      method: "PUT",
-      data,
-    });
-    return responseData;
-  };
+    const getAll = async (params) => {
+        const { data } = await api.request({
+            url: '/settings',
+            method: 'GET',
+            params
+        });
+        return data;
+    }
 
-  const get = async (param: string) => {
-    const { data } = await api.request({
-      url: `/setting/${param}`,
-      method: "GET",
-    });
-    return data;
-  };
+    const update = async (data) => {
+        const { data: responseData } = await api.request({
+            url: `/settings/${data.key}`,
+            method: 'PUT',
+            data
+        });
+        return responseData;
+    }
 
-  return {
-    getAll,
-    update,
-    get,
-  };
-};
+    return {
+		getAll,
+        update
+    }
+}
 
 export default useSettings;
--- frontend/src/pages/Queues/index.js
+++ frontend/src/pages/Queues/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState, useContext } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import {
   Button,
@@ -26,7 +26,6 @@ import QueueModal from "../../components/QueueModal";
 import { toast } from "react-toastify";
 import ConfirmationModal from "../../components/ConfirmationModal";
 import { socketConnection } from "../../services/socket";
-import { AuthContext } from "../../context/Auth/AuthContext";
 
 const useStyles = makeStyles((theme) => ({
   mainPaper: {
@@ -94,7 +93,6 @@ const Queues = () => {
   const [queueModalOpen, setQueueModalOpen] = useState(false);
   const [selectedQueue, setSelectedQueue] = useState(null);
   const [confirmModalOpen, setConfirmModalOpen] = useState(false);
-  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     (async () => {
@@ -112,8 +110,8 @@ const Queues = () => {
   }, []);
 
   useEffect(() => {
-    const companyId = user.companyId;
-    const socket = socketConnection({ companyId, userId: user.id });
+    const companyId = localStorage.getItem("companyId");
+    const socket = socketConnection({ companyId });
 
     socket.on(`company-${companyId}-queue`, (data) => {
       if (data.action === "update" || data.action === "create") {
@@ -165,7 +163,8 @@ const Queues = () => {
       <ConfirmationModal
         title={
           selectedQueue &&
-          `${i18n.t("queues.confirmationModal.deleteTitle")} ${selectedQueue.name
+          `${i18n.t("queues.confirmationModal.deleteTitle")} ${
+            selectedQueue.name
           }?`
         }
         open={confirmModalOpen}
@@ -178,16 +177,9 @@ const Queues = () => {
         open={queueModalOpen}
         onClose={handleCloseQueueModal}
         queueId={selectedQueue?.id}
-        onEdit={(res) => {
-          if (res) {
-            setTimeout(() => {
-              handleEditQueue(res)
-            }, 500)
-          }
-        }}
       />
       <MainHeader>
-        <Title>{i18n.t("queues.title")} ({queues.length})</Title>
+        <Title>{i18n.t("queues.title")}</Title>
         <MainHeaderButtonsWrapper>
           <Button
             variant="contained"
@@ -202,9 +194,6 @@ const Queues = () => {
         <Table size="small">
           <TableHead>
             <TableRow>
-              <TableCell align="center">
-                {i18n.t("queues.table.ID")}
-              </TableCell>
               <TableCell align="center">
                 {i18n.t("queues.table.name")}
               </TableCell>
@@ -226,7 +215,6 @@ const Queues = () => {
             <>
               {queues.map((queue) => (
                 <TableRow key={queue.id}>
-                  <TableCell align="center">{queue.id}</TableCell>
                   <TableCell align="center">{queue.name}</TableCell>
                   <TableCell align="center">
                     <div className={classes.customTableCell}>
